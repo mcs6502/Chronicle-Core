bd80663e66214806e5c732d8bf58418a22df0f2d
Igor Mironov <mcs6502-sek@yahoo.com.au>
2019-03-16 14:33:56 +1100

    fix race condition in JitterSamplerTest

--- src/test/java/net/openhft/chronicle/core/threads/JitterSamplerTest.java
+++ src/test/java/net/openhft/chronicle/core/threads/JitterSamplerTest.java
@@ -2,6 +2,9 @@ package net.openhft.chronicle.core.threads;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -9,14 +12,22 @@ public class JitterSamplerTest {
 
     @Test
     public void takeSnapshot() throws InterruptedException {
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch finishLatch = new CountDownLatch(1);
+
         Thread t = new Thread(() -> {
             JitterSampler.atStage("started");
+            startLatch.countDown();
             JitterSampler.sleepSilently(60);
+            await(finishLatch);
             JitterSampler.atStage("finishing");
             JitterSampler.sleepSilently(60);
             JitterSampler.finished();
         });
         t.start();
+
+        // wait for JitterSampler.desc to be primed as code below depends on it
+        await(startLatch);
 
         for (int i = 0; i < 10; i++) {
             JitterSampler.sleepSilently(10);
@@ -25,12 +36,24 @@ public class JitterSamplerTest {
             if ("finishing".equals(JitterSampler.desc)) {
                 if (s != null && s.contains("finish"))
                     break;
-            } else
+            } else {
                 assertTrue("started".equals(JitterSampler.desc));
-
+            }
+            finishLatch.countDown(); // release if not already released
         }
+
         t.join();
         String s = JitterSampler.takeSnapshot(10_000_000);
         assertNull(s);
+    }
+
+    private void await(CountDownLatch latch) {
+        boolean ok;
+        try {
+            ok = latch.await(5, SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        assertTrue(ok);
     }
 }
